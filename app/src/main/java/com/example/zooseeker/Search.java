481f3d1d2352a587e8d3f9085401b410b147f843
Hayk Khulyan <hkhulyan@gmail.com>
2022-05-01 18:01:13 -0700

    Add getMatchingExhibits function

--- app/src/main/java/com/example/zooseeker/Search.java
+++ app/src/main/java/com/example/zooseeker/Search.java
@@ -1,12 +1,14 @@
 package com.example.zooseeker;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Search {
     public static void displaySearchResult(String label) {
         // 2. Load the information about our nodes and edges...
         Map<String, ZooData.VertexInfo> vInfo = ZooData.loadVertexInfoJSON("app/src/main/assets/sample_node_info.json");
-        // Map<String, ZooData.EdgeInfo> eInfo = ZooData.loadEdgeInfoJSON("sample_edge_info.json");
 
         for(ZooData.VertexInfo x : vInfo.values()) {
             if (x.tags.contains(label) || x.name.contains(label)) {
@@ -14,4 +16,21 @@ public class Search {
             }
         }
     }
+
+    public static List<ZooData.VertexInfo> getMatchingExhibits(String label) {
+        // 2. Load the information about our nodes and edges...
+        Map<String, ZooData.VertexInfo> vInfo = ZooData.loadVertexInfoJSON("app/src/main/assets/sample_node_info.json");
+
+        // the map to hold our search results
+        List<ZooData.VertexInfo> results = new ArrayList<ZooData.VertexInfo>();
+
+        for(ZooData.VertexInfo x : vInfo.values()) {
+            if (x.tags.contains(label) || x.name.contains(label)) {
+                results.add(x);
+            }
+        }
+
+        return results;
+    }
+
 }
